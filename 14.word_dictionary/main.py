aeb05cbcfb1daae548ea8b4d8a8823d1e87b9ba7
AnjasFedo <fedoaf98@gmail.com>
2024-01-29 22:36:28 +0700

    Import PyDictionary

--- 14.word_dictionary/main.py
+++ 14.word_dictionary/main.py
@@ -1,16 +1,11 @@
-def main():
-    word_dictionary = {
-        "hai": "menyapa",
-        "mata": "indra pengelihatan",
-        'bumi': "planet dengan kehidupan"
-    }
-    
-    while True:
-        word = input("> Enter the word: ")
-        if word == "":
-            break
-        
-        if word in word_dictionary:
-            print(f"{word}: {word_dictionary[word]}")
-    
-main()
+from PyDictionary import PyDictionary
+
+dictionaries = PyDictionary()
+
+while True:
+    word = input("> Enter the word: ")
+
+    if word == "":
+        break
+
+    print(dictionaries.meaning(word))
