4e8139c2fa73a98f3f5ebf792a5e723b3cad6cb5
AnjasFedo <fedoaf98@gmail.com>
2024-01-30 21:31:06 +0700

    Rock Paper Scissors

---
+++ 15.rock-paper-scissors/main.py
@@ -0,0 +1,64 @@
+import random
+
+is_exit = False
+
+user_points = 0
+computer_points = 0
+
+def print_input(user_input, computer_input):
+    print(f"Your input is {user_input}")
+    print(f"Computer input is {computer_input}")
+
+while is_exit == False:
+    options = ["rock", "paper", "scissors"]
+
+    user_input = input("> Choose rock, paper, scissors or exit: ").lower()
+
+    computer_input = random.choice(options)
+    
+    if user_input not in options and user_input != "exit":
+        print("Invalid input")
+    
+    if user_input == "exit":
+        print("Game ended")
+        is_exit = True
+        
+    if user_input == "rock":
+        if computer_input == "rock":
+            print_input(user_input=user_input, computer_input=computer_input)
+            print("It is a tie")
+        elif computer_input == "paper":
+            print_input(user_input=user_input, computer_input=computer_input)
+            print("Computer win")
+            computer_points += 1
+        elif computer_input == "scissors":
+            print_input(user_input=user_input, computer_input=computer_input)
+            print("You win")
+            user_points += 1
+    elif user_input == "paper":
+        if computer_input == "paper":
+            print_input(user_input=user_input, computer_input=computer_input)
+            print("It is a tie")
+        elif computer_input == "scissors":
+            print_input(user_input=user_input, computer_input=computer_input)
+            print("Computer win")
+            computer_points += 1
+        elif computer_input == "rock":
+            print_input(user_input=user_input, computer_input=computer_input)
+            print("You win")
+            user_points += 1
+    elif user_input == "scissors":
+        if computer_input == "scissors":
+            print_input(user_input=user_input, computer_input=computer_input)
+            print("It is a tie")
+        elif computer_input == "rock":
+            print_input(user_input=user_input, computer_input=computer_input)
+            print("Computer win")
+            computer_points += 1
+        elif computer_input == "paper":
+            print_input(user_input=user_input, computer_input=computer_input)
+            print("You win")
+            user_points += 1
+        
+
+
