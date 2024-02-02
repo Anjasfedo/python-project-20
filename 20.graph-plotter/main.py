987604e8a35346600da3fdaecc91d7443f7c0667
AnjasFedo <fedoaf98@gmail.com>
2024-02-02 09:35:32 +0700

    Costumize the Plot Line

--- 20.graph-plotter/main.py
+++ 20.graph-plotter/main.py
@@ -3,12 +3,15 @@ import matplotlib.pyplot as plt
 x1 = [2, 4, 5, 6]
 y1 = [2, 3, 6, 7]
 
-plt.plot(x1, y1, label="Line 1")
+plt.plot(x1, y1, color="green", linestyle="dashed", linewidth="3", marker="o", markerfacecolor="blue", markersize=12, label="Line 1")
+
+plt.xlim(1,10)
+plt.ylim(1,10)
 
 x2 = [1, 2, 3, 4]
 y2 = [1, 2, 3, 4]
 
-plt.plot(x2, y2, label="Line 2")
+# plt.plot(x2, y2, label="Line 2")
 
 plt.xlabel("X Axis")
 plt.ylabel("Y Axis")
